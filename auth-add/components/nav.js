48f1fc6eb93c4cfed9261eb61944ea2011598e2a
vladian95 <kvankvan1995@gmail.com>
2023-10-17 16:23:06 +0300

    enter is done

--- auth-add/components/nav.js
+++ auth-add/components/nav.js
@@ -1,16 +1,26 @@
 import Link from 'next/link';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../utils/firebase';
 import '../src/styles/Home.module.css';
 
 const Nav = () => {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <nav className="navBar">
       <Link href={'/'} className="logo">
         Logo
       </Link>
-
-      <Link href={'/auth/login '} className="join">
-        join now
-      </Link>
+      {!user && (
+        <Link href={'/auth/login '} className="join">
+          join now
+        </Link>
+      )}
+      {user && (
+        <div>
+          <Link href={'/dashboard'}>welcome</Link>
+        </div>
+      )}
     </nav>
   );
 };
--- auth-add/src/pages/auth/login.js
+++ auth-add/src/pages/auth/login.js
@@ -1,9 +1,21 @@
 import { FcGoogle } from 'react-icons/fc';
 import { AiFillFacebook } from 'react-icons/ai';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../../utils/firebase';
 
 export default function Login() {
   //sign in with google
+
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log(result.user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="login">
       <h2>Join Today</h2>
@@ -11,7 +23,7 @@ export default function Login() {
         <h3 className="loginH3">Sign in with one of the providers</h3>
       </div>
       <div className="btnBox">
-        <button>
+        <button onClick={GoogleLogin}>
           <FcGoogle />
           <span>Sign in with Google</span>
         </button>
